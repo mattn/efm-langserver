4fb6eeace7a0f10b19813e776b4e746a50e05790
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-11-15 14:20:12 +0900

    Add missing file
    Closes #17

---
+++ langserver/handle_text_document_completion.go
@@ -0,0 +1,21 @@
+package langserver
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/sourcegraph/jsonrpc2"
+)
+
+func (h *langHandler) handleTextDocumentCompletion(ctx context.Context, conn *jsonrpc2.Conn, req *jsonrpc2.Request) (result interface{}, err error) {
+	if req.Params == nil {
+		return nil, &jsonrpc2.Error{Code: jsonrpc2.CodeInvalidParams}
+	}
+
+	var params CompletionParams
+	if err := json.Unmarshal(*req.Params, &params); err != nil {
+		return nil, err
+	}
+
+	return h.completion(params.TextDocument.URI, &params)
+}

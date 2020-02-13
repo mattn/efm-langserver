cc7bea3abb5905239d05d21ba8511d72945c8fc3
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2020-02-13 13:27:25 +0900

    Add tests

--- langserver/handler_test.go
+++ langserver/handler_test.go
@@ -1,10 +1,11 @@
 package langserver
 
 import (
+	"strings"
 	"testing"
 )
 
-func TestNoLinter(t *testing.T) {
+func TestLintNoLinter(t *testing.T) {
 	h := &langHandler{
 		configs: map[string][]Language{},
 		files: map[string]*File{
@@ -14,11 +15,11 @@ func TestNoLinter(t *testing.T) {
 
 	_, err := h.lint("file:///foo")
 	if err == nil {
-		t.Error("Should be an error if no linters")
+		t.Fatal("Should be an error if no linters")
 	}
 }
 
-func TestNoFileMatched(t *testing.T) {
+func TestLintNoFileMatched(t *testing.T) {
 	h := &langHandler{
 		configs: map[string][]Language{},
 		files: map[string]*File{
@@ -28,6 +29,47 @@ func TestNoFileMatched(t *testing.T) {
 
 	_, err := h.lint("file:///bar")
 	if err == nil {
-		t.Error("Should be an error if no linters")
+		t.Fatal("Should be an error if no linters")
+	}
+}
+
+func TestLintFileMatched(t *testing.T) {
+	h := &langHandler{
+		rootPath: "C:/",
+		configs: map[string][]Language{
+			"vim": {
+				{
+					LintCommand:        `echo C:/foo:2:No it is normal!`,
+					LintIgnoreExitCode: true,
+					LintStdin:          true,
+				},
+			},
+		},
+		files: map[string]*File{
+			"file:///C:/foo": &File{
+				LanguageID: "vim",
+				Text:       "scriptencoding utf-8\nabnormal!\n",
+			},
+		},
+	}
+
+	d, err := h.lint("file:///C:/foo")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(d) != 1 {
+		t.Fatal("diagnostics should be only one")
+	}
+	if d[0].Range.Start.Line != 1 {
+		t.Fatalf("range.start.line should be %v but got: %v", 1, d[0].Range.Start.Line)
+	}
+	if d[0].Range.Start.Character != 0 {
+		t.Fatalf("range.start.character should be %v but got: %v", 0, d[0].Range.Start.Character)
+	}
+	if d[0].Severity != 1 {
+		t.Fatalf("severity should be %v but got: %v", 0, d[0].Severity)
+	}
+	if strings.TrimSpace(d[0].Message) != "No it is normal!" {
+		t.Fatalf("message should be %q but got: %q", "No it is normal!", strings.TrimSpace(d[0].Message))
 	}
 }

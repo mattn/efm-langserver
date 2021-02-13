1be42ad5f5e843044c40e8880454dc3ba3272e04
atx <atx@drecks-provider.de>
2021-02-14 00:41:18 +0100

    remove non rune test again, can not happen due to format

--- langserver/handler.go
+++ langserver/handler.go
@@ -454,13 +454,7 @@ func (h *langHandler) lint(ctx context.Context, uri DocumentURI) ([]Diagnostic, 
 
 			// we allow the config to provide a mapping between LSP types E,W,I,N and whatever categories the linter has
 			if len(config.LintCategoryMap) > 0 {
-				runeMap := []rune(config.LintCategoryMap[string(entry.Type)])
-				entry.Type = runeMap[0]
-				// If the map of linter categories to LSP Types is like Refactoring: Info instead of Refactoring: I
-				// we default to I instead of the first character
-				if len(runeMap) > 1 {
-					entry.Type = 'I'
-				}
+				entry.Type = []rune(config.LintCategoryMap[string(entry.Type)])[0]
 			}
 
 			severity := 1
